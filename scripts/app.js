cb6d0508ec6fca9f74016a7c381cda57e67b6a8b
albertrozzz <albertpaul17112000@gmail.com>
2025-02-13 19:49:17 -0500

    comp-6062-lab-assessment-1

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+let studentFullName = 'Albert Paul';
+let studentNumber = 1190851;
+const result= studentFullName + ' - ' + studentNumber;
+console.log(result);
+let headerContenrt = document.querySelector("h1");
+headerContenrt.classList.add("headingPrimary");
+headerContenrt.classList.add();
